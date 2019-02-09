34a128646ae15d078fa31622fa5145665399508d
Abhilasha <jainabhilasha74@gmail.com>
2019-02-09 22:25:41 +0530

    First Commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by abhilasha on 11-12-2018.
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello Spring Boot";
+    }
+}
